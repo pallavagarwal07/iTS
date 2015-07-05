315fccb735c1b27f2ddb1af3353f7cb40bf5e83b
Pallav Agarwal <pallavag@iitk.ac.in>
2015-07-05 20:15:01 +0530

    String Diff algorithm added

---
+++ src/stringDiff.py
@@ -0,0 +1,53 @@
+arr = []
+def Dif(str1, str2):
+    if arr[len(str2)][len(str1)] != 999999:
+        return arr[len(str2)][len(str1)]
+    if len(str1) == 0:
+        arr[len(str2)][len(str1)] = len(str2)
+        return len(str2)
+    if len(str2) == 0:
+        arr[len(str2)][len(str1)] = len(str1)
+        return len(str1)
+    if str1[-1] == str2[-1]:
+        k = Dif(str1[:len(str1)-1], str2[:len(str2)-1])
+        arr[len(str2)][len(str1)] = k
+        return k
+    k = min(Dif(str1, str2[:len(str2)-1]), Dif(str1[:len(str1)-1], str2)) + 1
+    arr[len(str2)][len(str1)] = k
+    return k
+
+
+#def print_arr(arr):
+    #print "\t0\t",
+    #for a in range(1, len(arr[0])):
+        #print str1[a-1]+"\t",
+    #print ''
+    #for i, a in enumerate(arr):
+        #print str2[i-1] if i>0 else '0', "\t",
+        #for b in a:
+            #print str(b) + "\t",
+        #print ''
+    #print '\n\n'
+
+def getIndex(str2, str1, index):
+    len1 = len(str1)+1
+    len2 = len(str2)+1
+    global arr
+    arr = [999999]*len2
+    for i in range(0, len2):
+        arr[i] = [999999]*len1
+
+    Dif(str1, str2)
+    i = len2 - 1
+    j = len1 - 1
+
+    stack = {}
+
+    while i > 1 and j > 1:
+        k = sorted([[arr[i-1][j], (i-1, j)], [arr[i][j-1], (i, j-1)], [arr[i-1][j-1], (i-1, j-1)]])
+        k = k[0][1]
+        i, j = k
+        stack[i] = j
+
+    return stack[index+1]-1
+
