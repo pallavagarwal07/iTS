f5b34b1e5deed8189f348c0cd24e43c3d60afecf
Pallav Agarwal <pallavag@iitk.ac.in>
2015-07-07 16:13:59 +0530

    stringDiff made iterative

--- src/stringDiff.py
+++ src/stringDiff.py
@@ -1,33 +1,58 @@
 arr = []
+
 def Dif(str1, str2):
-    if arr[len(str2)][len(str1)] != 999999:
-        return arr[len(str2)][len(str1)]
-    if len(str1) == 0:
-        arr[len(str2)][len(str1)] = len(str2)
-        return len(str2)
-    if len(str2) == 0:
-        arr[len(str2)][len(str1)] = len(str1)
-        return len(str1)
-    if str1[-1] == str2[-1]:
-        k = Dif(str1[:len(str1)-1], str2[:len(str2)-1])
-        arr[len(str2)][len(str1)] = k
-        return k
-    k = min(Dif(str1, str2[:len(str2)-1]), Dif(str1[:len(str1)-1], str2)) + 1
-    arr[len(str2)][len(str1)] = k
-    return k
+    m = len(str2)
+    n = len(str1)
+    i = 0
+    j = 0
+    while i<=n:
+        arr[0][i] = i
+        i += 1
+    while j<=m:
+        arr[j][0] = j
+        j += 1
+    i = 1
+    j = 1
+    while i <= n:
+        j = 1
+        while j <= m:
+            if str2[j-1] == str1[i-1]:
+                arr[j][i] = arr[j-1][i-1]
+            else:
+                arr[j][i] = min(arr[j-1][i], arr[j][i-1]) + 1
+            j += 1
+        i += 1
+    return arr[m][n]
+
+#def Dif(str1, str2):
+    #if arr[len(str2)][len(str1)] != 999999:
+        #return arr[len(str2)][len(str1)]
+    #if len(str1) == 0:
+        #arr[len(str2)][len(str1)] = len(str2)
+        #return len(str2)
+    #if len(str2) == 0:
+        #arr[len(str2)][len(str1)] = len(str1)
+        #return len(str1)
+    #if str1[-1] == str2[-1]:
+        #k = Dif(str1[:len(str1)-1], str2[:len(str2)-1])
+        #arr[len(str2)][len(str1)] = k
+        #return k
+    #k = min(Dif(str1, str2[:len(str2)-1]), Dif(str1[:len(str1)-1], str2)) + 1
+    #arr[len(str2)][len(str1)] = k
+    #return k
 
 
-#def print_arr(arr):
-    #print "\t0\t",
-    #for a in range(1, len(arr[0])):
-        #print str1[a-1]+"\t",
-    #print ''
-    #for i, a in enumerate(arr):
-        #print str2[i-1] if i>0 else '0', "\t",
-        #for b in a:
-            #print str(b) + "\t",
-        #print ''
-    #print '\n\n'
+def print_arr(str2, str1, arr):
+    print "\t0\t",
+    for a in range(1, len(arr[0])):
+        print str1[a-1]+"\t",
+    print ''
+    for i, a in enumerate(arr):
+        print str2[i-1] if i>0 else '0', "\t",
+        for b in a:
+            print str(b) + "\t",
+        print ''
+    print '\n\n'
 
 stack = {}
 
